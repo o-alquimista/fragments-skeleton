e719d71a0f440ea9ecf7b78d3e130f83dee03529
Douglas Silva <doug.hs@protonmail.ch>
2020-10-10 23:49:57 -0300

    Compatibility with Fragments v0.8

--- public/index.php
+++ public/index.php
@@ -1,12 +1,27 @@
 <?php
 
 /**
- * The entry point of the application.
+ * The entrypoint of a Fragments application, the front controller.
  */
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Fragments\Component\Bootstrap;
+use Fragments\Component\Http\Request;
+use Fragments\Component\Http\Response;
+use Fragments\Component\Routing\Router;
+use Fragments\Bundle\Exception\NotFoundHttpException;
 
-$bootstrap = new Bootstrap;
-$bootstrap->run();
+$request = new Request();
+
+try {
+    $router = new Router();
+    $response = $router->run($request);
+} catch (NotFoundHttpException $e) {
+    $response = new Response('Page not found', 404);
+} catch (Throwable $e) {
+    error_log($e);
+
+    $response = new Response('Something went wrong.', 500);
+}
+
+$response->send();
