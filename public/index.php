f8eeb035a74a8657fd4bfb6fe2643d8590ee299a
Douglas Silva <doug.hs@protonmail.ch>
2020-11-07 17:19:46 -0300

    Catch RouteNotFoundException

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * The entrypoint of a Fragments application, the front controller.
+ * The entrypoint of a Fragments application: the front controller.
  */
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -9,18 +9,18 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Fragments\Component\Http\Request;
 use Fragments\Component\Http\Response;
 use Fragments\Component\Routing\Router;
-use Fragments\Bundle\Exception\NotFoundHttpException;
+use Fragments\Component\Routing\Exception\RouteNotFoundException;
 
 $request = new Request();
 
 try {
     $router = new Router();
     $response = $router->run($request);
-} catch (NotFoundHttpException $e) {
+} catch (RouteNotFoundException $e) {
     $response = new Response('Page not found', 404);
 } catch (Throwable $e) {
     error_log($e);
-
+    
     $response = new Response('Something went wrong.', 500);
 }
 
